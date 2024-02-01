620d68bb9217c56a174d697088cef991edf609c5
NasserMagumba <158153966+NasserMagumba@users.noreply.github.com>
2024-02-01 17:36:39 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,7 @@
+print("\t\|republic of uganda national id card\n")
+print("\t\tsurname:MAGUMBA\n")
+print("\t\tgivenname:IDD NASSER\n")
+print("\t\tnin220220275\n")
+print ("\t\tdate of birth:16/05/2002\n")
+print("\t\tgender:male\n")
+
---
+++ assignment2.py
@@ -0,0 +1,1 @@
+print("\t\tISLAMIC UNIVERSITY IN UGANDA \n\n\t\tKAMPALA CAMPUS \n\n\tname:\t\tMAGUMBA IDD NASSER\n\tReg:\t\t223-063012-27522\n\tCourse unit: \tprogramming \n\tLecture: \tHajji walusimbi \n\tdate:\t\t24-01-2023\ 
